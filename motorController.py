b1d61e7b68b03aa3c660d4225fe66ffd6b64ee3f
Fredrik Heiberg <fredrik.heiberg@yahoo.com>
2016-04-17 00:56:03 +0200

    separate motor file

---
+++ motorController.py
@@ -0,0 +1,21 @@
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+
+import time
+import atexit
+
+mh = Adafruit_MotorHAT(addr=0x60)
+
+def turnOffMotors():
+	mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+
+atexit.register(turnOffMotors)
+
+myMotor = mh.getMotor(3)
+
+myMotor.setSpeed(255)
+
+def mixDrink:
+	myMotor.run(Adafruit_MotorHAT.RELEASE);
+
+	myMotor.run(Adafruit_MotorHAT.FORWARD)
+	time.sleep(5.0)
