dc7ffe594807871170759cca21c91db6f3a98e3c
Fredrik Heiberg <fredrik.heiberg@yahoo.com>
2016-04-17 01:58:14 +0200

    function fix

--- app.py
+++ app.py
@@ -26,6 +26,11 @@ def index():
 			drinkSize = param.get('size')
 
 			mixDrink(drinkName, drinkSize)
+			myMotor.setSpeed(255)
+			myMotor.run(Adafruit_MotorHAT.RELEASE);
+			myMotor.run(Adafruit_MotorHAT.FORWARD)
+			time.sleep(5.0)
+			turnOffMotors()
 
 	return render_template('index.html', error=error)
 
@@ -83,11 +88,11 @@ def fantaVodka(size):
 	#time.sleep(5.0)
 
 # recommended for auto-disabling motors on shutdown!
-#def turnOffMotors():
-#	mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-#	mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-#	mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-#	mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+def turnOffMotors():
+	mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
  
 #atexit.register(turnOffMotors)
 
