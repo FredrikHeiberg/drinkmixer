0adc0a5bd0226b9a9cad0b6f1a74941fdb9e43ca
Fredrik Heiberg <fredrik.heiberg@yahoo.com>
2016-04-17 11:36:56 +0200

    change waypoint

--- app.py
+++ app.py
@@ -31,7 +31,6 @@ def index():
 
 @app.route('/process', methods=['GET', 'POST'])
 def process():
-	mixTheDrink()
 	return render_template('process.html')
 
 # @app.route('/mix', methods=['GET'])
@@ -51,6 +50,7 @@ def process():
 
 # Run Python code to select desired drink and size
 def mixDrink(name, size):
+	mixTheDrink()
 	print "run mixer: %s, %s" %(name, size)
 	#myMotor.setSpeed(255)
 	#myMotor.run(Adafruit_MotorHAT.FORWARD)
