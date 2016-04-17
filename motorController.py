c95fac8fe3e54e157dc1e234490c318d0f6e4140
Fredrik Heiberg <fredrik.heiberg@yahoo.com>
2016-04-17 11:50:54 +0200

    change waypoint

--- motorController.py
+++ motorController.py
@@ -3,7 +3,6 @@ from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 import time
 import atexit
 
-print "FILE RUN"
 
 mh = Adafruit_MotorHAT(addr=0x60)
 
@@ -18,6 +17,7 @@ atexit.register(turnOffMotors)
 myMotor = mh.getMotor(3)
 
 def mixTheDrink():
+	print "START MOTOR!"
 	myMotor.setSpeed(255)
 	myMotor.run(Adafruit_MotorHAT.RELEASE);
 
